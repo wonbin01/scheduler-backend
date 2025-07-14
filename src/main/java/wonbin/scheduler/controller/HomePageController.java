dcb1dfeed5b32e750687876e4892f716026b75bb
wonbin <wonbin3977@gmail.com>
2025-07-14 21:40:33 +0900

    로그인

---
+++ src/main/java/wonbin/scheduler/controller/HomePageController.java
@@ -0,0 +1,32 @@
+package wonbin.scheduler.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wonbin.scheduler.member.MemberInfo;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class HomePageController {
+    @GetMapping("/api/session")
+    public ResponseEntity<?> checkSession(HttpSession session){
+        MemberInfo loginMember=(MemberInfo) session.getAttribute("loginMember");
+
+        if(loginMember!=null){
+            log.info("Session 확인, memberId={}",loginMember.getUsername());
+            return ResponseEntity.ok(Map.of(
+                    "id", loginMember.getUsername()
+            ));
+        }
+        else
+        {
+            log.info("Session 확인 실패, 로그인 페이지로 이동");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 필요");
+        }
+    }
+}
--- src/main/java/wonbin/scheduler/controller/LoginController.java
+++ src/main/java/wonbin/scheduler/controller/LoginController.java
@@ -1,7 +1,10 @@
 package wonbin.scheduler.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wonbin.scheduler.Repository.MemoryRepository;
@@ -10,7 +13,6 @@ import wonbin.scheduler.member.MemberInfo;
 
 @RestController
 @Slf4j
-@CrossOrigin(origins ="https://localhost:3000")
 public class LoginController {
     Repository repository=new MemoryRepository(); //지금은 일단 메모리에 저장
     @PostMapping("/signup") //회원가입
@@ -24,8 +26,8 @@ public class LoginController {
         return ResponseEntity.ok("회원가입 성공");
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody MemberInfo info)
+    //@PostMapping("/login")
+    public ResponseEntity<String> loginV1(@RequestBody MemberInfo info)
     {
         log.info("로그인");
         int private_num=info.getUsername();
@@ -39,6 +41,33 @@ public class LoginController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("비밀번호가 틀렸습니다.");
 
         log.info("로그인 성공: {}",private_num);
-        return ResponseEntity.ok("로그인 성공");
+
+        ResponseCookie cookie=ResponseCookie.from("loginToken",String.valueOf(private_num))
+                .httpOnly(true) //javascript에서 접근 x
+                .path("/") //전체 경로에 대해서 쿠키 유효
+                .maxAge(3600) //1시간만 유효한 쿠키
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE,cookie.toString())
+                .body("로그인 성공");
     }
+    @PostMapping("/login")
+        public ResponseEntity<String> loginV2(@RequestBody MemberInfo info, HttpSession session){
+            log.info("로그인 요청 V2: {}",info.getUsername());
+            int private_num=info.getUsername();
+            String private_password=info.getPassword();
+            log.info("MemberId={} , Password={}", private_num,private_password);
+
+            MemberInfo check=repository.findById(private_num);
+            if(check==null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("존재하지 않는 아이디");
+
+            if(!check.getPassword().equals(private_password))
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("비밀번호가 틀렸습니다");
+
+            log.info("로그인 성공:{}",private_num);
+
+            session.setAttribute("loginMember",check);
+            return ResponseEntity.ok("로그인 성공");
+        }
 }
