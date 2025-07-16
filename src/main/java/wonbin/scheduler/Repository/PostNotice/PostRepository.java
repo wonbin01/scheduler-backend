c1387b4745a200811340304e14ef0492cb561850
wonbin <wonbin3977@gmail.com>
2025-07-17 02:21:37 +0900

    공지사항 상세글 보기 및 뒤로가기+삭제 구현

--- src/main/java/wonbin/scheduler/Repository/PostNotice/PostRepository.java
+++ src/main/java/wonbin/scheduler/Repository/PostNotice/PostRepository.java
@@ -3,9 +3,11 @@ package wonbin.scheduler.Repository.PostNotice;
 import wonbin.scheduler.Entity.Post.PostInfo;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository {
     public PostInfo save(String title,String content,String category);
     public List<PostInfo> findByCategory(String category);
-    public List<PostInfo> findAll();
+    public Optional<PostInfo> findById(int id);
+    public boolean DeleteById(int id);
 }
