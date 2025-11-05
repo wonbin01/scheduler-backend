46a63f7f3ea68d6362bc10dbb75902438de6679c
wonbin <wonbin3977@gmail.com>
2025-11-05 22:20:24 +0900

    feat(Service) : documentAIservice에서 문서를 api를 사용해 서버에 보내고, OCR로 추출된 정보를 받는 메서드 구현

--- src/main/java/wonbin/scheduler/Service/scheduleParsar/DocumentAiService.java
+++ src/main/java/wonbin/scheduler/Service/scheduleParsar/DocumentAiService.java
@@ -1,7 +1,15 @@
 package wonbin.scheduler.Service.scheduleParsar;
 
 
+import com.google.cloud.documentai.v1.Document;
+import com.google.cloud.documentai.v1.DocumentProcessorServiceClient;
+import com.google.cloud.documentai.v1.ProcessRequest;
+import com.google.cloud.documentai.v1.ProcessResponse;
+import com.google.cloud.documentai.v1.RawDocument;
+import com.google.protobuf.ByteString;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
 
 public class DocumentAiService {
     @Value("${gcp.project.id}")
@@ -11,4 +19,30 @@ public class DocumentAiService {
     @Value("${gcp.documentai.processor.location}")
     private String location;
 
+    public String extractText(MultipartFile file) throws IOException {
+        try (DocumentProcessorServiceClient client = DocumentProcessorServiceClient.create()) {
+            String processorName = String.format(
+                    "projects/%s/locations/%s/processors/%s",
+                    projectId, location, processorId
+            );
+            //파일 -> byteString 변환
+            ByteString content = ByteString.copyFrom(file.getBytes());
+
+            //요청 생성
+            RawDocument document = RawDocument.newBuilder()
+                    .setContent(content)
+                    .setMimeType(file.getContentType() != null ? file.getContentType() : "image/png")
+                    .build();
+
+            ProcessRequest request = ProcessRequest.newBuilder()
+                    .setName(processorName)
+                    .setRawDocument(document)
+                    .build();
+
+            ProcessResponse response = client.processDocument(request);
+            Document doc = response.getDocument();
+
+            return doc.getText();
+        }
+    }
 }
